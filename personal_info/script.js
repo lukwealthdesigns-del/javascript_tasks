a1c32521a1d2785d756be0255f199536d8487384
Lukman <lukwealthdesigns@gmail.com>
2025-09-22 10:01:01 +0100

    initial commit

--- personal_info/script.js
+++ personal_info/script.js
@@ -21,8 +21,11 @@ document.getElementById("btn-info").onclick = function() {
     displayInformation();
     
     let information = document.getElementById("information")
-    information.style.backgroundColor = "blue"
-    information.style.padding = @
+    information.style.backgroundColor = "white"
+    information.style.padding = "20px 40px"
+    information.style.border = "1px solid grey"
+    information.style.borderLeft = "3px solid green"
+    information.style.borderRadius = "10px"
 }
 
 
